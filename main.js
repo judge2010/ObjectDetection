ef8038f961e0e93266503848b9814a57cc9f821a
judge2010 <74088083+judge2010@users.noreply.github.com>
2021-05-18 16:11:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,6 +11,18 @@ function setup(){
 };
 function draw(){
  image(img ,0 ,0 ,600 ,400);
+ if(status!=""){
+     for(i=0;i<object.length;i++){
+        document.getElementById("status").innerHTML= "Status - Object Detected";
+        fill(255,0,0);
+        percentage=floor(object[i].confidence*100);
+        textSize(25);
+        text(object[i].label+" "+percentage+"%",object[i].x+70,object[i].y+100);
+        noFill();
+        stroke(255,0,0);
+        rect(object[i].x,object[i].y,object[i].width,object[i].height);
+     };
+ };
 };
 function modelLoaded(){
 console.log("model is loaded"); 
